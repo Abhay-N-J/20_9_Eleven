180e7619c5f6b35501f655be912d342939faf5a4
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-02-25 20:54:34 +0530

    Add product updating feature in backend

--- Server/routes/retailer.js
+++ Server/routes/retailer.js
@@ -9,40 +9,40 @@ const saltRounds = 10
 router.post('/signup', (request, response, next) => {
     try {
         bcrypt.hash(request.body.password, saltRounds)
-        .then(hash => {
-            const retailTemplate = new retailModel({
-                username: request.body.username,
-                name: request.body.name,
-                password: hash,
-                email: request.body.email,
-                location: request.body.location,
-                products: []
-            })
-            retailTemplate.save((err, data) => {
-                if (err) {
-                    if (err.code === 11000) {
-                        // Duplicate username
-                        return response.status(400).json({
-                            success: false,
-                            code: 11000,
-                            message: 'Username already exist!',
-                            errors: "Username already exists",
-                        });
-                    }
+            .then(hash => {
+                const retailTemplate = new retailModel({
+                    username: request.body.username,
+                    name: request.body.name,
+                    password: hash,
+                    email: request.body.email,
+                    location: request.body.location,
+                    products: []
+                })
+                retailTemplate.save((err, data) => {
+                    if (err) {
+                        if (err.code === 11000) {
+                            // Duplicate username
+                            return response.status(400).json({
+                                success: false,
+                                code: 11000,
+                                message: 'Username already exist!',
+                                errors: "Username already exists",
+                            });
+                        }
 
-                    // Some other error
-                    err.success = false;
-                    return response.status(400).json(err);
-                }
-                data.success = true;
-                response.json(data);
+                        // Some other error
+                        err.success = false;
+                        return response.status(400).json(err);
+                    }
+                    data.success = true;
+                    response.json(data);
+                })
             })
-        })
     }
-    catch(err) {
+    catch (err) {
         next(err);
     }
-    
+
 })
 
 router.post('/login', (request, response, next) => {
@@ -86,22 +86,56 @@ router.post('/login', (request, response, next) => {
     }
 })
 
-// router.get('/')
+router.get('/product', (req, res, next) => {
+    try {
+        let products = productModel.find()
+            .then((product) => {
+                res.send(products)
+            })
+    }
+    catch (err) {
+        next(err);
+    }
+})
 
+router.put('/product-update', (req, res, next) => {
+    try {
+        // Product updation fields coming
+        /*
+        product_id:
+        shop_id:
+        shop_name:
+        qty:
+        */
+        let product = productModel.findOne({ _id: req.body.product_id })
+            .then((product) => {
+                product.shops.forEach((shop) => {
+                    if (shop.shop_id === req.body.shop_id) {
+                        shop.quantity = req.body.qty
+                    }
+                })
+                product.save()
+                res.send(product)
+            })
+    }
+    catch (err) {
+        next(err);
+    }
+})
 
 
 router.put('/add-item', async (req, res, next) => {
     try {
-        let product = await productModel.findOne({name : req.body.name})
-        if(!product) {
+        let product = await productModel.findOne({ name: req.body.name })
+        if (!product) {
             product = new productModel({
                 name: req.body.name,
                 qty: req.body.qty,
                 price: req.body.price,
                 image_link: req.body.image_link,
                 shops: [{
-                    shop_id:req.body.shop_id,
-                    quantity:req.body.qty,
+                    shop_id: req.body.shop_id,
+                    quantity: req.body.qty,
                     shop_name: req.body.shop_name
                 }],
                 description: req.body.description
@@ -111,15 +145,15 @@ router.put('/add-item', async (req, res, next) => {
         }
         else {
             product.shops.push({
-                shop_id:req.body.shop_id,
-                quantity:req.body.qty,
+                shop_id: req.body.shop_id,
+                quantity: req.body.qty,
                 shop_name: req.body.shop_name
             })
             product.save()
             res.send(product)
         }
     }
-    catch(err) {
+    catch (err) {
         next(err);
     }
 })
